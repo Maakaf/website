5bd35e506203952d885755d0c5e4d01742b4acf3
israel zusha rotenberg <100128949+zushar@users.noreply.github.com>
2024-07-19 19:10:05 +0300

    refactor: Update Leaderboard.tsx to improve data rendering and filtering

--- components/Leaderboard/Leaderboard.tsx
+++ components/Leaderboard/Leaderboard.tsx
@@ -29,7 +29,7 @@ const LeaderboardPage: React.FC = () => {
   return (
     <>
       <h3 className="text-2xl font-bold mb-2 text-center">Our Contributions In Maakaf</h3>
-      <div className="sticky top-20 dark:bg-gray-700 z-10 shadow-md">
+      <div className="sticky top-20 dark:bg-gray-600 z-10 shadow-md">
         <div className="font-bold flex flex-row justify-center p-4">
           <a href={"#allTimes"} className="transition duration-300 group px-4">
             <span>{"All Times Contribution"}</span>
@@ -65,12 +65,12 @@ export const ContributionsList: React.FC<{ data: ContributorsInsights }> = ({ da
   const mappedData = data.members.map(mapMembers).filter(p => !p.name.includes('[bot]'));
 
   return (
-    <div className="font-inter container mx-auto p-4" id={data.stat === "allTimes" ? "allTimes" : data.stat === "lastMonth" ? "lastMonth" : "lastWeek"}>
+    <div className="font-inter container mx-auto m-4 bg-gray-600" id={data.stat === "allTimes" ? "allTimes" : data.stat === "lastMonth" ? "lastMonth" : "lastWeek"}>
       <div className="flex flex-row justify-center">
-        <h6 className="text-gray-400 mb-4 text-center ml-10">{formatSinceAndUntil(data.since, data.until)}</h6>
-        <h6 className='text-center'>{data.stat === "allTimes" ? "All Times" : data.stat === "lastMonth" ? "Last Month" : "Last Week"}</h6>
+        <h4 className="text-gray-400 mb-4 text-center ml-10">{formatSinceAndUntil(data.since, data.until)}</h4>
+        <h4 className='text-center'>{data.stat === "allTimes" ? "All Times" : data.stat === "lastMonth" ? "Last Month" : "Last Week"}</h4>
       </div>
-      <ul className="grid min:grid-cols-1 grid-cols-3 gap-3">
+      <ul className="grid grid-cols-3 gap-2">
         {mappedData.filter(p => p.score).map((data, ind) => (
           <li key={data.node_id}>
             <DisplayPerson2 data={data} place={ind + 1} />
@@ -87,7 +87,7 @@ export const DisplayPerson2: React.FC<PersonPlace> = ({ data, place }) => {
     .map(([key]) => key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()));
 
   return (
-    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden h-full">
       <div className="flex items-center px-6 py-4 bg-gray-900">
         <Image className="h-12 w-12 rounded-full object-cover" src={data.avatar_url} alt="avatar" width={48} height={48} />
         <div className="ml-4">
@@ -96,9 +96,6 @@ export const DisplayPerson2: React.FC<PersonPlace> = ({ data, place }) => {
         </div>
       </div>
       <div className="px-6 py-4">
-        <p className="text-gray-700 text-base">
-          <strong>Contribution percentage:</strong> {data.score}
-        </p>
         <div className="mt-4">
           <h3 className="text-gray-900 text-lg font-semibold">Contributions</h3>
           <ul className="mt-2 text-gray-700">
@@ -108,18 +105,18 @@ export const DisplayPerson2: React.FC<PersonPlace> = ({ data, place }) => {
           </ul>
         </div>
         <div className="mt-4">
-          <h3 className="text-gray-900 text-lg font-semibold">Insightful Contributions</h3>
-          <ul className="mt-2 text-gray-700">
-            {insights.map((insight, index) => (
-              <li key={index}>{insight}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="mt-4">
           <h3 className="text-gray-900 text-lg font-semibold">Projects Contributions</h3>
           <ul className="mt-2 text-gray-700">
             {data.projects_names.map((project, index) => (
               <li key={index}>{project.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-gray-900 text-lg font-semibold">Insightful Contributions</h3>
+          <ul className="mt-2 text-gray-700">
+            {insights.map((insight, index) => (
+              <li key={index}>{insight}</li>
             ))}
           </ul>
         </div>
